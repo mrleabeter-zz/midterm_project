cd70d0217755e500f7d4fae39f8049ac11a1a223
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-14 18:04:55 -0400

    Constructed sign up form

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/user/signup_successful'do
+  erb :'user/signup_successful'
+end
+
+post '/' do
+  @user = User.new(
+    username:   params[:username],
+    email: params[:email],
+    password:  params[:password]
+  )
+  @company = Company.new(
+    company_name:   params[:company_name],
+    url: params[:url]
+  )
+  @discount = Discount.new(
+    discount_percent:  params[:discount_percent]
+  )
+  @restriction = Restriction.new(
+    description:  params[:description]
+  )
+  if @user.save && company.save && discount.save && restriction.save
+    redirect '/user/signup_successful'
+  else
+    erb :index
+  end
+end
