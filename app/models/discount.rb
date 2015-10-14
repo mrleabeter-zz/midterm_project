ce9718b62508e426402492fbe7895d6f15e8794c
Christopher Leabeter <christopherleabeter@gmail.com>
2015-10-14 16:34:00 -0400

    Added validation to Discount table

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,10 @@
 class Discount < ActiveRecord::Base
 
+  belongs_to :company
+  belongs_to :user
+
+  validates :discount_percent,
+              numericality: { only_integer: true},
+              inclusion: { in: 1..100, message: "must be between 1 and 100"}
+
 end
