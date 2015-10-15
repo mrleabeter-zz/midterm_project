70e39a9b6d82286f775c0e3067f476b51e2f0686
jackiechong <jackiekchong@mail.com>
2015-10-15 12:30:52 -0400

    removed company url

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,12 +5,4 @@ class Company < ActiveRecord::Base
   validates :company_name,
               presence: true
 
-  validate :valid_company_url
-
-private
-
-  def valid_company_url
-    errors.add(:url_not_valid, "must be in format http(s)://www.website.com") unless url =~ /^(https|http)?:\/\/[\S]+/
-  end
-
 end
