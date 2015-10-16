ff0b6b5dbebf9c46b11f2fea7e0f75d07089d44d
jackiechong <jackiekchong@mail.com>
2015-10-16 15:50:37 -0400

    minor

--- app/actions.rb
+++ app/actions.rb
@@ -23,7 +23,6 @@ end
 get '/user/profile' do
   @user = User.find(session[:user_id])
   @companies = Company.all
-  @discounts = Discount.all
   erb :'user/profile'
 end
 
@@ -96,16 +95,6 @@ post '/user/login' do
   end
 end
 
-# get '/user/:discount_id/restriction' do
-#   @discount = Discount.find(params[:discount_id])
-#   erb :'user/restriction'
-# end
-#
-# get '/user/:user_id/discount' do
-#   @user = User.find(params[:user_id])
-#   erb :'user/discount'
-# end
-
 post '/user/update_profile' do
   @user = User.find(session[:user_id])
   @user.update(
@@ -119,54 +108,5 @@ post '/user/update_profile' do
     redirect '/user/profile'
   else
     erb :index
-  end
-end
-
-post '/user/:discount_id/restriction' do
-  @discount = Discount.find(params[:discount_id])
-  @restriction = Restriction.new(
-    discount_id: @discount.id,
-    description: params[:description]
-    )
-  if @restriction.save
-    redirect '/user/profile'
-  else
-    redirect '/user/profile'
-  end
-end
-
-post '/user/:user_id/discount' do
-  @user = User.find(session[:user_id])
-
-  @existing_company = Company.find_by(company_name: params[:company_name])
-  if @existing_company
-    @discount = Discount.find_by(params[:discount_id])
-    @restriction = Restriction.new(
-      discount_id: @discount.id,
-      description: params[:description]
-      )
-  else
-    @company = Company.new(
-      company_name:   params[:company_name]
-    )
-    @company.save
-  end
-
-  @discount = Discount.new(
-    user_id: @user.id,
-    company_id: @company.id,
-    discount_percent: params[:discount_percent]
-    )
-
-  if @company.save
-    @discount.save
-    @restriction = Restriction.new(
-      discount_id: @discount.id,
-      description: params[:description]
-      )
-    @restriction.save
-    redirect '/user/profile'
-  else
-    redirect "/user/profile"
   end
 end
